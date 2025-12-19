9de2e99239c7c4956f6d735d453d7170efbb34e5
Lucas Mettetal <lucas.mettetal2@gmail.com>
2025-12-19 11:20:51 +0000

    Refactor exec.c: enhance split_line function and improve run_cmd command execution

--- exec.c
+++ exec.c
@@ -1,46 +1,95 @@
 #include "shell.h"
 
 /**
- * run_cmd - executes a command using fork and execve.
- * It creates a child process to run the command and waits for it to finish.
- * @cmd: the command to execute
- * @sh: pointer to the shell state
- * Return: exit status of the command
+ * split_line - Split a line into tokens (argv style).
+ * @line: Input line (modified in place).
+ *
+ * Return: NULL-terminated array of tokens, or NULL on failure.
  */
+char **split_line(char *line)
+{
+	size_t cap = 8, i = 0;
+	char **argv, **tmp;
+	char *tok;
 
-int run_cmd(char *cmd, t_shell *sh)
+	if (!line)
+		return (NULL);
+
+	argv = malloc(sizeof(char *) * cap);
+	if (!argv)
+		return (NULL);
+
+	tok = strtok(line, " \t");
+	while (tok)
+	{
+		argv[i++] = tok;
+
+		if (i + 1 >= cap)
+		{
+			cap *= 2;
+			tmp = realloc(argv, sizeof(char *) * cap);
+			if (!tmp)
+			{
+				free(argv);
+				return (NULL);
+			}
+			argv = tmp;
+		}
+
+		tok = strtok(NULL, " \t");
+	}
+	argv[i] = NULL;
+
+	return (argv);
+}
+
+/**
+ * run_cmd - Execute a command line using fork and execve.
+ * @line: Full input line (command + arguments). Modified in place.
+ * @sh: Shell state.
+ *
+ * Return: Exit status of the command.
+ */
+int run_cmd(char *line, t_shell *sh)
 {
 	pid_t pid;
 	int status;
-	char *args[2];
+	char **argv;
 
-	args[0] = cmd;
-	args[1] = NULL;
+	argv = split_line(line);
+	if (!argv || !argv[0])
+	{
+		free(argv);
+		return (sh->status);
+	}
 
 	pid = fork();
 	if (pid < 0)
 	{
 		perror("fork");
+		free(argv);
 		sh->status = 1;
 		return (1);
 	}
 
 	if (pid == 0)
 	{
-		execve(cmd, args, sh->env);
+		execve(argv[0], argv, sh->env);
 
-		/* execve failed */
 		fprintf(stderr, "%s: %lu: %s: not found\n",
-				sh->prog, sh->lineno, cmd);
+				sh->prog, sh->lineno, argv[0]);
 		_exit(127);
 	}
 
 	if (waitpid(pid, &status, 0) < 0)
 	{
 		perror("waitpid");
+		free(argv);
 		sh->status = 1;
 		return (1);
 	}
+
+	free(argv);
 
 	if (WIFEXITED(status))
 		sh->status = WEXITSTATUS(status);
